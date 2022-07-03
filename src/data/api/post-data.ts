3ee46c581f278a4b113f525ff583ad260b666aab
agustinVon <agustin.von@ing.austral.edu.ar>
2022-07-03 17:27:50 -0300

    added endpoints

--- src/data/api/post-data.ts
+++ src/data/api/post-data.ts
@@ -1,5 +1,6 @@
+import { User } from './../users';
+import axios from 'axios';
 import { FullPost, NewPost, Post, PostData } from './../posts';
-import { axios } from 'axios';
 const jjAxios = axios.create({
     baseURL: "http://localhost:8081",
     headers: {
@@ -7,25 +8,38 @@ const jjAxios = axios.create({
     }
 })
 
+const mockUser:User = {
+    id: "string",
+    displayName: "string",
+    username: "string",
+    avatar: "string",
+    bio: "string"
+}
+
+const mockPost:Post = {
+    id: "",
+    text: "",
+    user: mockUser
+}
 export class PostApi implements PostData {
     answerPost(postId: string, answer: NewPost): Promise<FullPost> {
-        return jjAxios.post<NewPost, FullPost>(`${postId}/reply`, ({content: answer.text, username: answer.user}))
+        return jjAxios.post<NewPost, FullPost>(`${postId}/reply`, ({content: answer.text}))
     }
 
     createPost(post: NewPost): Promise<Post> {
-        
+        return new Promise(() => mockPost)
     }
 
     getFeedPosts(): Promise<Post[]> {
-        
+        return new Promise(() => mockPost)
     }
 
     getFullPostById(id: string): Promise<FullPost | undefined> {
-        
+        return new Promise(() => mockPost)
     }
 
     getPostsByUser(userId: string): Promise<Post[]> {
-        
+        return new Promise(() => mockPost)
     }
 
 }
